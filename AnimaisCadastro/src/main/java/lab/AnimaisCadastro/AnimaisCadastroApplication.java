48a996e606bf8a4067ab3b4e2d7bf398b0ad33ee
Antony Guilherme <83239872+antonycabral@users.noreply.github.com>
2024-08-20 15:38:27 -0300

    feat add projeto base

---
+++ AnimaisCadastro/src/main/java/lab/AnimaisCadastro/AnimaisCadastroApplication.java
@@ -0,0 +1,13 @@
+package lab.AnimaisCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaisCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaisCadastroApplication.class, args);
+	}
+
+}
---
+++ AnimaisCadastro/src/test/java/lab/AnimaisCadastro/AnimaisCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.AnimaisCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaisCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
